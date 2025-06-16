352c0b0417cb8a2ed02da1b95510f078225d9fcc
riteshk-007 <codeshorts007@gmail.com>
2025-06-16 10:29:46 +0530

    feat: refactor server initialization and enhance ecosystem configuration for clarity

--- client/server.js
+++ client/server.js
@@ -1,25 +1,36 @@
 import { createServer } from "http";
-import next from "next";
+import { Server } from "next/dist/server/next-server.js";
+import { join } from "path";
 import dotenv from "dotenv";
 
 // Load environment variables
 dotenv.config();
 
-const port = process.env.PORT || 3001;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const port = parseInt(process.env.PORT || "3001", 10);
+const hostname = process.env.HOSTNAME || "localhost";
+const dir = join(process.cwd());
+
+const app = new Server({
+  hostname,
+  port,
+  dir,
+  dev: false,
+  conf: {
+    env: {},
+    output: 'standalone',
+  },
+});
+
 const handle = app.getRequestHandler();
 
-app
-  .prepare()
-  .then(() => {
-    createServer((req, res) => {
-      handle(req, res); // Handle the requests
-    }).listen(port, (err) => {
-      if (err) throw err;
-      console.log(`> Ready on http://localhost:${port}`);
-    });
-  })
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, hostname, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+})
   .catch((err) => {
     console.error("Error starting server:", err);
     process.exit(1);
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  apps: [
-    {
-      name: "pro-housing-client",
-      cwd: "./client",
-      script: "node",
-      args: ".next/standalone/server.js",
-      env: {
-        NODE_ENV: "production",
-        PORT: "3001",
-        HOSTNAME: "localhost"
-      },
-    },
-    {
-      name: "pro-housing-server",
-      cwd: "./server",
-      script: "npm",
-      args: "start",
-      env: {
-        NODE_ENV: "production",
-        PORT: "4001"
-      },
-    },
-  ],
+    apps: [
+        {
+            name: "pro-housing-client",
+            cwd: "./client",
+            script: "node",
+            args: ".next/standalone/server.js",
+            env: {
+                NODE_ENV: "production",
+                PORT: "3001",
+                HOSTNAME: "localhost"
+            },
+        },
+        {
+            name: "pro-housing-server",
+            cwd: "./server",
+            script: "npm",
+            args: "start",
+            env: {
+                NODE_ENV: "production",
+                PORT: "4001"
+            },
+        },
+    ],
 };
